9f088c36abee3d1428acb16b58a14a9a3820a150
a-nihad <a.nihadkarulai@gmail.com>
2023-12-12 17:23:22 +0530

    Added React-Query, Fetching data, and Deleting Cabin,

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,18 +1,19 @@
-import { useEffect } from "react";
+import CabinTable from "../features/cabins/CabinTable.jsx";
 import Heading from "../ui/Heading.jsx";
 import Row from "../ui/Row";
-import { getCabins } from "../services/apiCabins.js";
 
 function Cabins() {
-  useEffect(function () {
-    getCabins().then((data) => console.log(data));
-  }, []);
-
   return (
-    <Row type="horizontal">
-      <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
-    </Row>
+    <>
+      <Row type="horizontal">
+        <Heading as="h1">All cabins</Heading>
+        <p>Filter / Sort</p>
+      </Row>
+
+      <Row>
+        <CabinTable />
+      </Row>
+    </>
   );
 }
 
